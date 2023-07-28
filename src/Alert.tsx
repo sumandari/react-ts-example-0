515852270054570ed39f9c5af0060bed5e51d06f
Sumandari <sumandari.ee@gmail.com>
2023-07-28 11:15:25 +0200

    added Alert.css

--- src/Alert.tsx
+++ src/Alert.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { useState } from 'react';
+import './Alert.css';
 
 type Props = {
   type?: string;
@@ -9,13 +10,7 @@ type Props = {
   onClose?: () => void;
 };
 
-export function Alert({
-  type = 'information',
-  heading,
-  children,
-  closable,
-  onClose
-}: Props) {
+export function Alert({ type = 'information', heading, children, closable, onClose }: Props) {
   const [visible, setVisible] = useState<boolean>(true);
   if (!visible) {
     return null;
@@ -27,24 +22,26 @@ export function Alert({
     }
   }
   return (
-    <div>
-      <div>
+    <div className={`container ${type}`}>
+      <div className="header">
         <span
           role="img"
-          aria-label={type === "warning" ? "Warning" : "Information"}
+          className="header-icon"
+          aria-label={type === 'warning' ? 'Warning' : 'Information'}
         >
-          {type === "warning" ? "⚠" : "ℹ️"}
+          {type === 'warning' ? '⚠' : 'ℹ️'}
         </span>
-        <span>{heading}</span>
+        <span className="header-text">{heading}</span>
+        {closable && (
+          <button className="close-button" aria-label="Close" onClick={handleCloseClick}>
+            <span role="img" aria-label="Close">
+              ❌
+            </span>
+          </button>
+        )}
       </div>
-      {closable && (
-        <button aria-label="Close" onClick={handleCloseClick}>
-          <span role="img" aria-label="Close">
-            ❌
-          </span>
-        </button>
-      )}
-      <div>{children}</div>
+
+      <div className="content">{children}</div>
     </div>
   );
 }
