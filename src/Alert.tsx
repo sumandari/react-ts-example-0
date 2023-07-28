f4bb913871872dbb0e5de66021574e21ad534c7b
Sumandari <sumandari.ee@gmail.com>
2023-07-29 00:08:38 +0200

    use @emotion/react css-in-js

--- src/Alert.tsx
+++ src/Alert.tsx
@@ -1,6 +1,7 @@
-/* eslint-disable prettier/prettier */
+/** @jsxImportSource @emotion/react  */
+import { css } from '@emotion/react';
+
 import { useState } from 'react';
-import styles from './Alert.module.css';
 
 type Props = {
   type?: string;
@@ -22,18 +23,52 @@ export function Alert({ type = 'information', heading, children, closable, onClo
     }
   }
   return (
-    <div className={`${styles.container} ${styles[type]}`}>
-      <div className={styles.header}>
+    <div
+      css={css`
+        display: inline-flex;
+        flex-direction: column;
+        text-align: left;
+        padding: 10px 15px;
+        border-radius: 4px;
+        border: 1px solid transparent;
+        color: ${type === 'warning' ? '#e7650f' : '#118da0'};
+        background-color: ${type === 'warning' ? '##f3e8da' : '#dcf1f3'};
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          margin-bottom: 5px;
+        `}
+      >
         <span
           role="img"
-          className={styles.headerIcon}
           aria-label={type === 'warning' ? 'Warning' : 'Information'}
+          css={css`
+            width: 30px;
+          `}
         >
           {type === 'warning' ? '⚠' : 'ℹ️'}
         </span>
-        <span className={styles.headerText}>{heading}</span>
+        <span
+          css={css`
+            font-weight: bold;
+          `}
+        >
+          {heading}
+        </span>
         {closable && (
-          <button className={styles.closeButton} aria-label="Close" onClick={handleCloseClick}>
+          <button
+            aria-label="Close"
+            onClick={handleCloseClick}
+            css={css`
+              border: none;
+              background: transparent;
+              margin-left: auto;
+              cursor: pointer;
+            `}
+          >
             <span role="img" aria-label="Close">
               ❌
             </span>
@@ -41,7 +76,14 @@ export function Alert({ type = 'information', heading, children, closable, onClo
         )}
       </div>
 
-      <div className="content">{children}</div>
+      <div
+        css={css`
+          margin-left: 30px;
+          color: #000;
+        `}
+      >
+        {children}
+      </div>
     </div>
   );
 }
